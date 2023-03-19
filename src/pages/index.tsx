f36ad96e3bd1211efbc6c767598015ee66b3e668
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-03-19 13:33:51 -0400

    fix env setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "y/utils/api";
 import { useEffect, useState } from "react";
--- src/server/api/routers/weather.ts
+++ src/server/api/routers/weather.ts
@@ -1,20 +1,25 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-import { createTRPCRouter, publicProcedure } from "y/server/api/trpc";
-import type {OpenWeatherResponse} from "y/server/api/types/OpenWeather";
+import { createTRPCRouter, publicProcedure } from 'y/server/api/trpc';
+import type { OpenWeatherResponse } from 'y/server/api/types/OpenWeather';
+import { env } from 'y/env.mjs';
 
 export const weatherRouter = createTRPCRouter({
-  getWeather: publicProcedure
-    .input(z.object({ lat: z.number(), lon: z.number() }))
-    .query(async ({ input }) => {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${input.lat}&lon=${input.lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
-        return await response.json() as Promise<OpenWeatherResponse>
-    }),
+    getWeather: publicProcedure
+        .input(z.object({ lat: z.number(), lon: z.number() }))
+        .query(async ({ input }) => {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${input.lat}&lon=${input.lon}&appid=${env.OPEN_WEATHER_API_KEY}`
+            );
+            return (await response.json()) as Promise<OpenWeatherResponse>;
+        }),
 
     getLatLongFromCityAndState: publicProcedure
         .input(z.object({ city: z.string(), state: z.string() }))
         .query(async ({ input }) => {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.city},${input.state}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
-            return response.json()
-        })
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${input.city},${input.state}&appid=${env.OPEN_WEATHER_API_KEY}`
+            );
+            return response.json();
+        }),
 });
